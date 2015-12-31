59b0a854213a5c0bf37976a5ce34e00375d10b93
David Sundelius <mail@davidsundelius.se>
2015-12-31 01:11:54 +0100

    Inital commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>DavidSundelius.se</title>
+<meta name="google-site-verification" content="qR0jmfRCNfMJe0y7LDEiEKKyn4l3rgHDGml2iiYdaaM" />
+</head><body bgcolor="#FFFFFF">
+<div id="main">
+<b>David Sundelius</b><br>
+MSc. Computer Science - algorithm, languages and logic from Chalmers University of Technology<br>
+Certified Professional Scrum Master I (PSM I)<br>
+Consultant for Knowit System Development<br><br>
+<i>Contact information:</i><br>
+<a href="mailto:mail@davidsundelius.se">mail@davidsundelius.se</a><br>
+(+46)73-2435711<br><br>
+<a href="../linkedin/"><img src="img/Linkedin.png"></a>
+<a href="../facebook/"><img src="img/Facebook.png"></a>
+<a href="../twitter/"><img src="img/Twitter.png"></a>
+<a href="../github/"><img src="img/Github.png"></a>
+<br><br>
+<i>� Copyright David Sundelius 2015</i>
+</body>
+</html>
